43de2cfa3fae4dd06c3fb3b065225196387825c5
orweis <orweis@gmail.com>
2020-12-09 12:06:25 +0200

    add support for single topic notify

--- event_notifier.py
+++ event_notifier.py
@@ -31,7 +31,7 @@ class EventNotifier:
 
     Usage example:
         notifier = EventNotifier()
-        
+
         #subscriber
         notifier.subscribe( notifier.gen_subscriber_id(), ["dinner is served", "breakfast is served"],
                             lambda topic, data: print(f"{topic}, let's eat. We have: {data}") )
@@ -56,7 +56,7 @@ class EventNotifier:
         """
         Subscribe to a set of topics.
         Once a notification (i.e. publish) of a topic is received the provided callback function will be called (with topic and data)
-        
+
 
         Args:
             subscriber_id (SubscriberID): A UUID identifying the subscriber
@@ -93,14 +93,18 @@ class EventNotifier:
                 subscribers = self._topics[topic]
                 del subscribers[subscriber_id]
 
-    async def notify(self, topics: TopicList, data=None):
+    async def notify(self, topics: Union[TopicList, Topic], data=None):
         """
         Notify subscribers of a new event per topic. (i.e. Publish events)
 
         Args:
-            topics (TopicList): Topics to trigger a publish event for (Calling the callbacks of all their subscribers)
+            topics (Union[TopicList, Topic]): Topics to trigger a publish event for (Calling the callbacks of all their subscribers)
             data ([type], optional): Arbitrary data to pass each callback. Defaults to None.
         """
+        # allow caller to pass a single topic without a list
+        if isinstance(topics, Topic):
+            topics = [topics]
+
         # TODO improve with reader/writer lock pattern - so multiple notifications can happen at once
         async with self.lock:
             for topic in topics:
