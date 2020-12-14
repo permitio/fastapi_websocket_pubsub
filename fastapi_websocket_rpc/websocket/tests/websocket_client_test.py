cf3faf1661f185f89abb5a54fabc677b2bc71ca3
orweis <orweis@gmail.com>
2020-12-14 14:25:32 +0200

    Add token veriying example top test

--- fastapi_websocket_rpc/websocket/tests/websocket_client_test.py
+++ fastapi_websocket_rpc/websocket/tests/websocket_client_test.py
@@ -5,16 +5,17 @@ from fastapi_websocket_rpc.websocket.websocket_rpc_client import WebSocketRpcCli
 
 
 async def run_client(uri):
-    async with  WebSocketRpcClient(uri, RpcUtilityMethods()) as client:
+    async with WebSocketRpcClient(uri, RpcUtilityMethods(), extra_headers=[("X-TOKEN", "fake-super-secret-token")]) as client:
         response = await client.other.echo(text="Hello World!")
-        print (response)
+        print(response)
         response = await client.call("get_proccess_details")
-        print (response)
-        response = await client.call("echo_method", {"method_name":"get_proccess_details", "args":{}})
-        print (response)
+        print(response)
+        response = await client.call("echo_method", {"method_name": "get_proccess_details", "args": {}})
+        print(response)
         # wait for server to call us and we reply
         await asyncio.sleep(1000)
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(run_client("ws://localhost:8000/ws/a3"))
+    asyncio.get_event_loop().run_until_complete(
+        run_client("ws://localhost:8000/ws/a3"))
