5242b64e51813a1f5d41790a05fd0c9eed47e819
orweis <orweis@gmail.com>
2021-02-02 22:53:09 +0200

    tests

---
+++ tests/basic_test.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+from fastapi_websocket_rpc import logger
+
+# Add parent path to use local src as package for tests
+sys.path.append(os.path.abspath(os.path.join(os.path.basename(__file__), os.path.pardir)))
+
+import asyncio
+from multiprocessing import Process
+import requests
+
+import pytest
+import uvicorn
+from fastapi import APIRouter, FastAPI
+
+
+from fastapi_websocket_rpc.logger import get_logger
+from fastapi_websocket_rpc.utils import gen_uid
+from fastapi_websocket_pubsub import PubSubEndpoint, PubSubClient
+
+logger = get_logger("Test")
+
+# Configurable
+PORT = int(os.environ.get("PORT") or "7990")
+uri = f"ws://localhost:{PORT}/pubsub"
+trigger_url = f"http://localhost:{PORT}/trigger"
+
+DATA = "MAGIC"
+EVENT_TOPIC = "event/has-happened"
+
+def setup_server_rest_route(app, endpoint: PubSubEndpoint):
+
+        @app.get("/trigger")
+        async def trigger_events():
+            logger.info("Triggered via HTTP route - publishing event")
+            # Publish an event named 'steel'
+            # Since we are calling back (RPC) to the client- this would deadlock if we wait on it
+            asyncio.create_task(endpoint.publish([EVENT_TOPIC], data=DATA))      
+            return "triggered"
+
+
+def setup_server():
+    app =  FastAPI()
+    router = APIRouter()
+    # PubSub websocket endpoint
+    endpoint = PubSubEndpoint()
+    endpoint.register_route(router, "/pubsub")
+    # Regular REST endpoint - that publishes to PubSub
+    setup_server_rest_route(app,endpoint)
+    app.include_router(router)
+    uvicorn.run(app, port=PORT)
+
+
+@pytest.fixture()
+def server():
+    # Run the server as a separate process
+    proc = Process(target=setup_server, args=(), daemon=True)
+    proc.start()
+    yield proc
+    proc.kill() # Cleanup after test
+
+@pytest.mark.asyncio
+async def test_subscribe_http_trigger(server):
+    # finish trigger
+    finish = asyncio.Event()
+    # Create a client and subscribe to topics
+    client = PubSubClient()
+    async def on_event(data):
+        assert data == DATA
+        finish.set()
+    # subscribe for the event
+    client.subscribe(EVENT_TOPIC, on_event)
+    # start listentining
+    client.start_client(uri)
+    # wait for the client to be ready to receive events 
+    await client.wait_until_ready()
+    # trigger the server via an HTTP route
+    requests.get(trigger_url)
+    # wait for finish trigger
+    await asyncio.wait_for(finish.wait(),5)
+
+@pytest.mark.asyncio
+async def test_pub_sub(server):
+    # finish trigger
+    finish = asyncio.Event()
+    # Create a client and subscribe to topics
+    client = PubSubClient()
+    async def on_event(data):
+        assert data == DATA
+        finish.set()
+    # subscribe for the event
+    client.subscribe(EVENT_TOPIC, on_event)
+    # start listentining
+    client.start_client(uri)
+    # wait for the client to be ready to receive events 
+    await client.wait_until_ready()
+    # publish events (with sync=False toa void deadlocks waiting on the publish to ourselves)
+    published = await client.publish([EVENT_TOPIC], data=DATA, sync=False, notifier_id=gen_uid())
+    assert published.result == True
+    # wait for finish trigger
+    await asyncio.wait_for(finish.wait(),5)
---
+++ tests/multiprocess_test.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+from fastapi_websocket_rpc import logger
+
+# Add parent path to use local src as package for tests
+sys.path.append(os.path.abspath(os.path.join(os.path.basename(__file__), os.path.pardir)))
+
+import asyncio
+from multiprocessing import Process, Event as ProcEvent
+import requests
+
+import pytest
+import uvicorn
+from fastapi import APIRouter, FastAPI
+
+
+from fastapi_websocket_rpc.logger import get_logger
+from fastapi_websocket_rpc.utils import gen_uid
+from fastapi_websocket_pubsub import PubSubEndpoint, PubSubClient
+
+logger = get_logger("Test")
+
+# Configurable
+PORT = int(os.environ.get("PORT") or "7990")
+uri = f"ws://localhost:{PORT}/pubsub"
+trigger_url = f"http://localhost:{PORT}/trigger"
+
+DATA = "MAGIC"
+EVENT_TOPIC = "event/has-happened"
+
+
+def setup_server():
+    app =  FastAPI()
+    router = APIRouter()
+    # PubSub websocket endpoint
+    endpoint = PubSubEndpoint()
+    endpoint.register_route(router, "/pubsub")
+    app.include_router(router)
+    uvicorn.run(app, port=PORT)
+
+def setup_publishing_client():
+    """
+    this client will publish an event to the main-test client 
+    """
+    async def actual():
+        # Give the other client a chance to subscribe (TODO: sync start between the processes)
+        await asyncio.sleep(2)
+        # Create a client and subscribe to topics
+        client = PubSubClient()
+        client.start_client(uri)
+        # wait for the client to be ready 
+        await client.wait_until_ready()
+        # publish event
+        logger.info("Publishing event")
+        published = await client.publish([EVENT_TOPIC], data=DATA)
+        assert published.result == True
+    logger.info("Starting async publishing client")
+    asyncio.get_event_loop().run_until_complete(actual())
+
+
+@pytest.fixture(scope="module")
+def server():
+    # Run the server as a separate process
+    proc = Process(target=setup_server, args=(), daemon=True)
+    proc.start()
+    yield proc
+    proc.kill() # Cleanup after test
+
+@pytest.fixture(scope="module")
+def pub_client():
+    # Run the server as a separate process
+    proc = Process(target=setup_publishing_client, args=(), daemon=True)
+    proc.start()
+    yield proc
+    proc.kill() # Cleanup after test
+
+
+@pytest.mark.asyncio
+async def test_pub_sub_multi_client(server, pub_client):
+    # finish trigger
+    finish = asyncio.Event()
+    # Create a client and subscribe to topics
+    client = PubSubClient()
+    async def on_event(data):
+        assert data == DATA
+        finish.set()
+    # subscribe for the event
+    logger.info("Subscribing for events")
+    client.subscribe(EVENT_TOPIC, on_event)
+    # start listentining
+    client.start_client(uri)
+    # wait for finish trigger
+    await asyncio.wait_for(finish.wait(),5)
