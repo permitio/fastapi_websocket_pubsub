db08f6852c0978c1eeeeb70e98379b113e31ca77
orweis <orweis@gmail.com>
2021-03-10 14:36:09 +0200

    Expose Subscription for ease of use

--- fastapi_websocket_pubsub/__init__.py
+++ fastapi_websocket_pubsub/__init__.py
@@ -1,4 +1,4 @@
 from .pub_sub_server import PubSubEndpoint
 from .pub_sub_client import PubSubClient
 from .event_broadcaster import EventBroadcaster, EventBroadcasterException
-from .event_notifier import EventNotifier, SubscriberId , SubscriptionId, Topic, TopicList
+from .event_notifier import EventNotifier, SubscriberId , SubscriptionId, Topic, TopicList, Subscription
