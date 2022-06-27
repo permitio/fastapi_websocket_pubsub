9a1ee2e8c22fae53b85d1e34fea497fba734b9da
Ro'e Katz <roekatz@gmail.com>
2022-06-27 17:15:01 +0300

    Introduce test_ws_closes_on_pg_broadcaster_disconnect (still not functional)
    This would become functional when we add a new postgres service that has the idle_timeout set

--- tests/broadcaster_test.py
+++ tests/broadcaster_test.py
@@ -4,6 +4,8 @@ import pytest
 import asyncio
 import uvicorn
 import requests
+import asyncpg
+import contextlib
 
 from typing import Optional
 from fastapi import FastAPI
@@ -31,10 +33,10 @@ second_endpoint_uri = f"ws://localhost:{PORT}/ws2"
 
 DATA = "MAGIC"
 EVENT_TOPIC = "event/has-happened"
-
+PG_URI = "postgres://postgres:postgres@localhost:5432"
 
 def setup_pubsub_endpoint(app, path: str):
-    endpoint = PubSubEndpoint(broadcaster="postgres://postgres:postgres@localhost:5432/")
+    endpoint = PubSubEndpoint(broadcaster=PG_URI)
 
     @app.websocket(path)
     async def websocket_rpc_endpoint(websocket: WebSocket):
@@ -62,6 +64,21 @@ def setup_server():
     print("Running server app")
     uvicorn.run(app, port=PORT)
 
+
+@pytest.fixture()
+def postgres():
+    os.system("docker rm -f broadcastdb")
+    os.system('docker run -i --rm postgres:alpine cat /usr/local/share/postgresql/postgresql.conf.sample > postgres.conf')
+    
+    # Set idle timeout of 5s in order to test completion of reader task
+    with open("postgres.conf", "a") as conf:
+        conf.writelines(["idle_session_timeout=5000", 
+                         "idle_in_transaction_session_timeout=5000"])
+                         
+    os.system('docker run -d -p 5433:5432 --name broadcastdb -v "$PWD/postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:alpine')
+    
+    yield "postgres://postgres:postgres@localhost:5433/"
+    os.system("docker rm -f broadcastdb")
 
 @pytest.fixture()
 def server():
@@ -110,3 +127,26 @@ async def test_all_clients_get_a_topic_via_broadcast(server):
 
             print("Wait for events to be set")
             await asyncio.wait_for(wait_for_sem(), 5)
+
+@pytest.mark.asyncio
+async def test_ws_closes_on_pg_broadcaster_disconnect(server):
+    """
+    if:
+    - A client is connected via websocket to a server
+    - The server is connected to a broadcaster backed by postgresql DB
+    - The server <-> DB connection gets disconnected 
+
+    then:
+    - Websocket connection between client and server should close
+    """
+    # TODO: This should use a different postgres DB that has 3s idle timeout
+
+    async def on_event(data, topic):
+        logger.info(f"client received data '{data}' on topic '{topic}'")
+
+    async with PubSubClient(topics=[EVENT_TOPIC], callback=on_event, server_uri=first_endpoint_uri) as c:
+        done, pending = await asyncio.wait([c._run_task], timeout=10)
+        # assert c._run_task in done
+        assert c._run_task in pending # This is without the fix
+
+    
