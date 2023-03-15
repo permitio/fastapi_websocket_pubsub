25e62b6200ed3ba9475863508c34ba0768837e04
roekatz <roe@roekatz.com>
2023-03-15 19:47:53 +0200

    bump version: 0.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='fastapi_websocket_pubsub',
-    version='0.3.1',
+    version='0.3.2',
     author='Or Weis',
     author_email="or@permit.io",
     description="A fast and durable PubSub channel over Websockets (using fastapi-websockets-rpc).",
